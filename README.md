578945f0c052079a965f81196697248bed506593
Melissa Brewer <melramirez727@gmail.com>
2021-05-31 21:01:44 -0500

    edit every hr for local storage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # work-day-scheduler
+Description:
+This is an 8am to 5pm hourly work day scheduler. Each hour has a text box and dats is saved to the local storage. 
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
-// current date in header
-var date = moment().format('dddd, MMMM Do YYYY');
-$("#currentDay").text(date);
 
-// saveBtn and save to local storage
 $(document).ready(function () {
-         $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
 
-    
+    $("#currentDay").text(moment().format("MMMM Do YYYY")); 
+
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+
         localStorage.setItem(time, text);
     })
-   
+    
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
 
+    
 })
