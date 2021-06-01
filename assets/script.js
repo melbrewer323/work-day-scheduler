d0125a406dc7fd02056cc07aa63d0077490a12ee
Melissa Brewer <melramirez727@gmail.com>
2021-05-31 19:57:03 -0500

    saveBtn and save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
-var time = moment().format('dddd, MMMM Do YYYY');
-$("#currentDay").text(time);
+// current date in header
+var date = moment().format('dddd, MMMM Do YYYY');
+$("#currentDay").text(date);
+
+// saveBtn and save to local storage
+$(document).ready(function () {
+         $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+    
+        localStorage.setItem(time, text);
+    })
+   
+
+})
